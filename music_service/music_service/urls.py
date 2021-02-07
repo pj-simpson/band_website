45a946e1e6564047519432cd5a7c7efcaea595da
Peter Simpson <peter_joseph_simpson@hotmail.com>
2021-02-07 19:46:10 +0000

    no longer checking in react build

--- music_service/music_service/urls.py
+++ music_service/music_service/urls.py
@@ -1,31 +1,16 @@
-"""untitled URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from drf_yasg import openapi  # new
-from drf_yasg.views import get_schema_view  # new
-from rest_framework import permissions  # new
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
-schema_view = get_schema_view(  # new
+schema_view = get_schema_view(
     openapi.Info(
         title="Music Service",
         default_version="v1",
-        description="A service to provide data to a musical project aplication",
+        description="A service to provide data to a musical project application",
         contact=openapi.Contact(email="peter_joseph_simpson@hotmail.com"),
     ),
     public=True,
